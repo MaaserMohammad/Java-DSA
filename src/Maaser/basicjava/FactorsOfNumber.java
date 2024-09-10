4e7b6d5b77641255ceca0b327ada03a1e7855c15
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-10 23:55:04 +0530

    Initial commit  of basic java   code

---
+++ src/Maaser/basicjava/FactorsOfNumber.java
@@ -0,0 +1,19 @@
+package Maaser.basicjava;
+
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+        public static void main(String[] args) {
+            Scanner  in = new Scanner(System.in);
+            System.out.print("Enter a positive integer: ");
+            int number = in.nextInt();
+            System.out.println("Factors of " + number + ":");
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
+
