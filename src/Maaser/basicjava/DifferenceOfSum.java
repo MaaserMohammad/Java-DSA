8551c841eb838677e7b33cd370518e97b90f7f08
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-10 22:03:38 +0530

    Initial commit  of basic java   code

--- src/Maaser/basicjava/DifferenceOfSum.java
+++ src/Maaser/basicjava/DifferenceOfSum.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class DifferenceOfSum {
     public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        int n=234;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n= in.nextInt();
         int sum=0;
         int product=1;
         while(n>0){
@@ -13,6 +14,7 @@ public class DifferenceOfSum {
             product *= n % 10;
             n /=10;
         }
+        System.out.print("This is the difference of their Product and Sum of given number: ");
         System.out.println(product-sum);
     }
 }
