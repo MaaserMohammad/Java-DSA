2f8d6f5e749d9cdff320fa6301ded707c7035761
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-13 22:56:39 +0530

    initial commits of basics java code.

---
+++ src/Maaser/basicjava/FactorialProgram.java
@@ -0,0 +1,11 @@
+package Maaser.basicjava;
+
+import java.util.Scanner;
+
+public class FactorialProgram {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+
+    }
+
+}
