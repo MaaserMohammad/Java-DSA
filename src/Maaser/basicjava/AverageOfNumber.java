dce8df69981bfefacb0e373ed5256796492ac0b6
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-16 16:39:04 +0530

    initial commits of basics java code.

--- src/Maaser/basicjava/AverageOfNumber.java
+++ src/Maaser/basicjava/AverageOfNumber.java
@@ -1,7 +1,20 @@
 package Maaser.basicjava;
 
+import java.util.Scanner;
+
 public class AverageOfNumber {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number you want to calculate the average: ");
+        int num= in.nextInt();
+        double sum =0;
+        for (int i=1; i <= num; i++){
+            System.out.print("Enter number " +i+ ": ");
+            double number= in.nextDouble();
+            sum += number;
+        }
+        double avg = sum/ num;
+        System.out.println("The average of the " +num+ " number is: " +avg);
 
     }
 }
