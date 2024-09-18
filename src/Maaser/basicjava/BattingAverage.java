ea8b783fd8e493360fde2040eb311afca19d9b73
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-18 23:05:46 +0530

    initial commits in the basics java code

--- src/Maaser/basicjava/BattingAverage.java
+++ src/Maaser/basicjava/BattingAverage.java
@@ -1,4 +1,9 @@
 package Maaser.basicjava;
 
+import java.util.Scanner;
+
 public class BattingAverage {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
 }
