09da310c6a93d58ab1a12560f85c30414c445511
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-20 22:25:34 +0530

    initial commits in the basics java code

--- src/Maaser/basicjava/BattingAverage.java
+++ src/Maaser/basicjava/BattingAverage.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class BattingAverage {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num= in.nextInt();
+        System.out.print("Enter the total run: ");
+        int totalrun= in.nextInt();
+        System.out.print("Enter the number of the innings: ");
+        int innings = in.nextInt();
 
+        double battingAvg= totalrun / innings ;
+        System.out.println("The Avg of the batting is " +battingAvg);
     }
 }
