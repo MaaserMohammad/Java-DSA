d27909f4f5fe5d4f0909ef839dbcef4e5af7e351
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-20 22:35:38 +0530

    initial commits in the basics java code

--- src/Maaser/basicjava/CgpaCalculation.java
+++ src/Maaser/basicjava/CgpaCalculation.java
@@ -1,9 +1,22 @@
 package Maaser.basicjava;
 
+import java.util.Scanner;
+
 public class CgpaCalculation {
 
     //Calculate CGPA in Java Program
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of Subject: ");
+        int numSubject= in.nextInt();
+
+        double totalCGPA= in.nextDouble();
+        for(int i = 1; i <= numSubject; i++){
+            System.out.print("Enter the markes/grade of the Subject " +i + ": ");
+            double grade=
+        }
+
+
 
     }
 }
