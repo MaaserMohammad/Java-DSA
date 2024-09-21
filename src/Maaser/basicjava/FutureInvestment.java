3f016d020a38a0c6f1dc6309bf189b7743d824bd
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-21 18:59:40 +0530

    initial commits in the basics java code

---
+++ src/Maaser/basicjava/FutureInvestment.java
@@ -0,0 +1,21 @@
+package Maaser.basicjava;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class FutureInvestment {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(" Enter the Principal Amount (initial Investment): ");
+        double principal = in.nextDouble();
+
+        System.out.print("Enter the Annual rate (in percentage): ");
+        double intrestrate = in.nextDouble() / 100;
+
+        System.out.print("Enter the number of the years: ");
+        int years = in.nextInt();
+
+        double futurevalue= principal * Math.pow((1+intrestrate),years);
+        System.out.printf("Future investment of the given value:%.2f\n", futurevalue);
+    }
+}
