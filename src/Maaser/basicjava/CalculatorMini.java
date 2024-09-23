fe1edea87c27b50920cf7750af747c606b2147ea
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-23 23:42:05 +0530

    initial commits in the basics java code

--- src/Maaser/basicjava/CalculatorMini.java
+++ src/Maaser/basicjava/CalculatorMini.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class CalculatorMini {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int sumNegative = 0;
+        int sumPositiveEven = 0;
+        int sumPositiveOdd = 0;
 
+        System.out.print("Let's calculate some sums! Enter numbers one by one (enter 0 to finish):");
+        while (true) {
+            int number = in.nextInt();
+            if (number == 0) {
+                break;
+            }
+            if (number < 0) {
+                sumNegative += number;
+            } else {
+                if (number % 2 == 0) {
+                    sumPositiveEven += number;
+                } else {
+                    sumPositiveOdd += number;
+                }
+            }
+        }
+        System.out.println("Sum of negative numbers: " + sumNegative);
+        System.out.println("Sum of positive even numbers: " + sumPositiveEven);
+        System.out.println("Sum of positive odd numbers: " + sumPositiveOdd);
     }
 }
+
