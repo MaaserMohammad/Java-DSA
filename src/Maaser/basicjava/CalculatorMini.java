4d96cd014ebd51e5500aade8f6eedfec77f9f917
MaaserMohammad <mohammadmaaser143@gmail.com>
2024-09-23 23:34:14 +0530

    initial commits in the basics java code

---
+++ src/Maaser/basicjava/CalculatorMini.java
@@ -0,0 +1,10 @@
+package Maaser.basicjava;
+
+import java.util.Scanner;
+
+public class CalculatorMini {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
