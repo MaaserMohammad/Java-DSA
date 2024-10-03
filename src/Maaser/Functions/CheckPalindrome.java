a0c4c0e6bf11970cdfea54543e3c39bc9d6c038f
Maaser <mohammadmaaser143@gmail.com>
2024-10-03 21:33:16 +0530

    basic commit in java function

--- src/Maaser/Functions/CheckPalindrome.java
+++ src/Maaser/Functions/CheckPalindrome.java
@@ -3,21 +3,21 @@ package Maaser.Functions;
 import java.util.Scanner;
 
 public class CheckPalindrome {
-    public static boolean checkPalindrome(int n) {
+    public static boolean checkPalindrome(int num) {
         int reverse = 0;
-        int temp = n;
+        int temp = num;
         while (temp != 0) {
             reverse = (reverse * 10) + (temp % 10);
             temp = temp / 10;
         }
-        return (reverse == n);
+        return (reverse == num);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = in.nextInt();
-        if (checkPalindrome(n)) {
+        System.out.print("Enter the number you want : ");
+        int num = in.nextInt();
+        if (checkPalindrome(num)) {
             System.out.println("Yes! it is a palindrome number.");
         } else {
             System.out.println("No! it is not palindrome number.");
