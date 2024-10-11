eae1516cf801949856c4580fbb268126235e981c
Maaser <mohammadmaaser143@gmail.com>
2024-10-11 23:15:11 +0530

    Update

---
+++ src/Maaser/LibraryManagement.java
@@ -0,0 +1,10 @@
+package Maaser;
+
+import java.util.Scanner;
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
