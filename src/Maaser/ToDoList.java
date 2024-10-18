ab1ea8d5e61bf1dccba44769beeb3168c5c4bbf7
Maaser <mohammadmaaser143@gmail.com>
2024-10-18 22:49:08 +0530

    Update

--- src/Maaser/ToDoList.java
+++ src/Maaser/ToDoList.java
@@ -17,7 +17,7 @@ public class ToDoList {
 
         // Menu-driven program
         do {
-            System.out.println("\n===== To-Do List Menu =====");
+            System.out.println("\n=== To-Do List Menu === ");
             System.out.println("1. ➕ Add Task");
             System.out.println("2. \uD83D\uDCCB View Tasks");
             System.out.println("3. ✅ Mark Task as Done");
