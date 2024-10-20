98e1aec076144ecf533b9cbca8dad10ad2fb9e6e
Maaser <mohammadmaaser143@gmail.com>
2024-10-20 23:50:15 +0530

    working on it

--- src/Maaser/LibraryManagement.java
+++ src/Maaser/LibraryManagement.java
@@ -2,9 +2,17 @@ package Maaser;
 
 import java.util.Scanner;
 
+
+class Book {
+    String title;
+    String author;
+    boolean isIssued;
+
 public class LibraryManagement {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Library library = new Library();
+        boolean exit = false;
 
     }
 }
