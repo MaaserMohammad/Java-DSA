415f8ec54e1eae06b65f33de2ae61b920cf7c60d
Maaser <mohammadmaaser143@gmail.com>
2024-10-21 22:20:24 +0530

    update and process on code

--- src/Maaser/LibraryManagement.java
+++ src/Maaser/LibraryManagement.java
@@ -1,5 +1,6 @@
 package Maaser;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,6 +8,21 @@ class Book {
     String title;
     String author;
     boolean isIssued;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+}
+
+class Library{
+    ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+}
 
 public class LibraryManagement {
     public static void main(String[] args) {
