518a47207096dc2ac8ac9a2cb49f938cbf7749b8
Maaser <mohammadmaaser143@gmail.com>
2024-10-24 23:46:44 +0530

    working on it

--- src/Maaser/UnitConverter.java
+++ src/Maaser/UnitConverter.java
@@ -4,4 +4,10 @@ public class UnitConverter {
     public static double kilometersToMiles(double kilometers) {
         return kilometers * 0.621371;
     }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+
 }
