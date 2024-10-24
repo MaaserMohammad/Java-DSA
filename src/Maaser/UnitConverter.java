72981a575b3afe82ee50701caea282d73a9bf846
Maaser <mohammadmaaser143@gmail.com>
2024-10-24 23:48:24 +0530

    working on it

--- src/Maaser/UnitConverter.java
+++ src/Maaser/UnitConverter.java
@@ -1,5 +1,7 @@
 package Maaser;
 
+import java.util.Scanner;
+
 public class UnitConverter {
     public static double kilometersToMiles(double kilometers) {
         return kilometers * 0.621371;
@@ -12,4 +14,12 @@ public class UnitConverter {
     public static double gramsToPounds(double grams) {
         return grams * 0.00220462;
     }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose conversion type: ");
+        System.out.println("1. Kilometers to Miles");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Grams to Pounds");
 }
