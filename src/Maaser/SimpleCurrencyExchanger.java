0931e1d033c2d248dcdc144cf585a90eedeae695
Maaser <mohammadmaaser143@gmail.com>
2024-11-05 22:08:45 +0530

    code Updated.

--- src/Maaser/SimpleCurrencyExchanger.java
+++ src/Maaser/SimpleCurrencyExchanger.java
@@ -9,7 +9,17 @@ public class SimpleCurrencyExchanger {
         System.out.print("Indian Currency Amount:");
         double rupee= input.nextDouble();
 
-        //exchange rates
+          //exchange rates
+        double usdExchangeRate = 83.9;
+        double eurExchangeRate = 92.0;
+        double gbpExchangeRate = 105.0;
+
+        //Calculating conversion
+        double dollar = rupee / usdExchangeRate;
+        double euro = rupee / eurExchangeRate;
+        double pound = rupee / gbpExchangeRate;
+
+
 
 
 
