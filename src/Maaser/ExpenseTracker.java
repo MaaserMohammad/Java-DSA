71d05293e1e1f42c36dab090105514a79a366da0
Maaser <mohammadmaaser143@gmail.com>
2024-11-10 23:20:37 +0530

    code Updated.

--- src/Maaser/ExpenseTracker.java
+++ src/Maaser/ExpenseTracker.java
@@ -1,5 +1,17 @@
 package Maaser;
 
+import java.util.Scanner;
+
 public class ExpenseTracker {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nExpense Tracker");
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Expenses");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 }
